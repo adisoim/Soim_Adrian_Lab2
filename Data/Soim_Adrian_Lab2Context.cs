79ec48d5703a3c3cf3960b007e836e36a9cb5faa
Adrian Soim <adisoim2002@gmail.com>
2023-10-31 13:28:00 +0200

    Add project files.

---
+++ Data/Soim_Adrian_Lab2Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Soim_Adrian_Lab2.Models;
+
+namespace Soim_Adrian_Lab2.Data
+{
+    public class Soim_Adrian_Lab2Context : DbContext
+    {
+        public Soim_Adrian_Lab2Context (DbContextOptions<Soim_Adrian_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Soim_Adrian_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Soim_Adrian_Lab2.Models.Publisher>? Publisher { get; set; }
+
+        public DbSet<Soim_Adrian_Lab2.Models.Author>? Author { get; set; }
+    }
+}
---
+++ Pages/Books/Create.cshtml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Soim_Adrian_Lab2.Data;
+using Soim_Adrian_Lab2.Models;
+
+namespace Soim_Adrian_Lab2.Pages.Books
+{
+    public class CreateModel : PageModel
+    {
+        private readonly Soim_Adrian_Lab2.Data.Soim_Adrian_Lab2Context _context;
+
+        public CreateModel(Soim_Adrian_Lab2.Data.Soim_Adrian_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            ViewData["PublisherID"] = new SelectList(_context.Set<Publisher>(), "ID",
+"PublisherName");
+            ViewData["AuthorID"] = new SelectList(_context.Set<Author>(), "ID",
+"LastName");
+            return Page();
+        }
+
+        [BindProperty]
+        public Book Book { get; set; } = default!;
+        
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+          if (!ModelState.IsValid || _context.Book == null || Book == null)
+            {
+                return Page();
+            }
+
+            _context.Book.Add(Book);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
